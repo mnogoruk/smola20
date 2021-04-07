6c4014a45fdd69c8a71ed320402bab348377910e
mnogoruk <lionless072@gmail.com>
2021-04-07 23:35:47 +0300

    Logging service in utils

---
+++ utils/service/loging.py
@@ -0,0 +1,34 @@
+import logging
+
+
+class LoggerService:
+    logger = None
+
+    @classmethod
+    def init_logger(cls):
+        cls.logger = logging.getLogger(f"{cls.__module__}:{cls.__name__}")
+
+    @classmethod
+    def check_logger(cls):
+        if cls.logger is None:
+            cls.init_logger()
+
+    @classmethod
+    def warning(cls, msg,  **kwargs):
+        cls.check_logger()
+        cls.logger.warning(msg,  **kwargs)
+
+    @classmethod
+    def error(cls, msg,  **kwargs):
+        cls.check_logger()
+        cls.logger.error(msg,  **kwargs)
+
+    @classmethod
+    def info(cls, msg, **kwargs):
+        cls.check_logger()
+        cls.logger.info(msg, **kwargs)
+
+    @classmethod
+    def debug(cls, msg,  **kwargs):
+        cls.check_logger()
+        cls.logger.debug(msg,  **kwargs)
